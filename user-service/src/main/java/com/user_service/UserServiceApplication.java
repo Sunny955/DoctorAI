38af55b7cc21818a43386a2058d1f56d3c1db356
amiya.ranjan <amiya.ranjan@emizainc.com>
2024-12-26 00:04:04 +0530

    Added .env file and updated properties file

--- user-service/src/main/java/com/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/user_service/UserServiceApplication.java
@@ -1,5 +1,6 @@
 package com.user_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("/Users/shippigo/Documents/DoctorAI/user-service") // Absolute path to the user-service folder
+				.load();
+
+		// Add environment variables to System properties
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
